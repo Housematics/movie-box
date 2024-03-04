cbd9b313a6cc2283f61d5c88a16daa8fc6e71402
Housematics <houssem.developer@gmail.com>
2024-03-04 20:41:53 +0100

    add some tmdb apis

--- src/api/movies.api.ts
+++ src/api/movies.api.ts
@@ -4,6 +4,8 @@ import type { Trending, Gender } from "@interfaces/movies.interface";
 export const fetchGenders = async (type: Gender) =>
   axios.get(`/genre/${type}/list`);
 
+export const fetchLanguages = async () => axios.get("/configuration/languages");
+
 export const fetchTrendings = async (type: Trending) =>
   axios.get(`/trending/${type}/day`);
 
@@ -16,8 +18,8 @@ export const fetchById = async (type: Gender, id: number) =>
 export const fetchTrailers = async (type: Gender, id: number) =>
   axios.get(`/${type}/${id}/videos`);
 
-export const fetchCelebrities = async (filter: string) =>
-  axios.get(`/person/popular?${filter}`);
+export const fetchCelebrities = async (page: string) =>
+  axios.get(`/person/popular?page=${page}`);
 
 export const fetchCelebrity = async (id: number, filter: string) =>
   axios.get(`/person/popular/${id}?${filter}`);
