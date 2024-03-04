5ad378efab7404527af20eb779d0fb6238c6754c
Housematics <houssem.developer@gmail.com>
2024-03-04 21:06:39 +0100

    add some changes to the layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Header from "@components/header/Header";
+import Footer from "@components/footer/Footer";
 
-const Layout = () => {
-  return <div>Layout</div>;
+interface Props {
+  children: JSX.Element | JSX.Element[];
+}
+
+const Layout: React.FC<Props> = ({ children }) => {
+  return (
+    <React.Fragment>
+      <Header />
+      {children}
+      <Footer />
+    </React.Fragment>
+  );
 };
 
 export default Layout;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,13 +2,13 @@ import { RouteObject } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import Signup from "@pages/auth/signup/Signup";
 import Signin from "@pages/auth/signin/Signin";
-import Celebrities from "@pages/app/Celebrities/Celebrities";
-import Celebrity from "@pages/app/Celebrity/Celebrity";
-import Home from "@pages/app/Home/Home";
-import Movie from "@pages/app/Movie/Movie";
-import Movies from "@pages/app/Movies/Movies";
-import Profile from "@pages/app/Profile/Profile";
-import Saved from "@pages/app/Saved/Saved";
+import Celebrities from "@/pages/app/celebrities/Celebrities";
+import Celebrity from "@/pages/app/celebrity/Celebrity";
+import Home from "@/pages/app/home/Home";
+import Movie from "@/pages/app/movie/Movie";
+import Movies from "@/pages/app/movies/Movies";
+import Profile from "@/pages/app/profile/Profile";
+import Saved from "@/pages/app/saved/Saved";
 
 export const routes: RouteObject[] = [
   {
