d913ef14a85fde6990a37e8b6c8de8587c27f4f5
Housematics <houssem.developer@gmail.com>
2024-03-04 22:58:00 +0100

    add default values in tmdb api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import routes from "@routes/routes";
-
 const App: React.FC = () => {
   return (
     <React.Fragment>
--- src/api/movies.api.ts
+++ src/api/movies.api.ts
@@ -9,7 +9,7 @@ export const fetchLanguages = async () => axios.get("/configuration/languages");
 export const fetchTrendings = async (type: Trending) =>
   axios.get(`/trending/${type}/day`);
 
-export const fetchMovies = async (type: Gender, filter: string) =>
+export const fetchMovies = async (type: Gender, filter: string = "") =>
   axios.get(`/discover/${type}?${filter}`);
 
 export const fetchById = async (type: Gender, id: number) =>
@@ -21,5 +21,5 @@ export const fetchTrailers = async (type: Gender, id: number) =>
 export const fetchCelebrities = async (page: string) =>
   axios.get(`/person/popular?page=${page}`);
 
-export const fetchCelebrity = async (id: number, filter: string) =>
+export const fetchCelebrity = async (id: number, filter: string = "") =>
   axios.get(`/person/popular/${id}?${filter}`);
