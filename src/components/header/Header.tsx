1dc01c54339fb71e31d6c7af400195f33f262c46
Housematics <houssem.developer@gmail.com>
2024-03-05 15:27:30 +0100

    add header structure

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./Heder.css";
+import Logo from "@assets/tv.png";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
     <header className="header" id="header">
-      Header
+      <Link to="/">
+        <img src={Logo} alt="tv-logo" />
+        <h2>MovieBox</h2>
+      </Link>
+
+      <input type="text" placeholder="What do you want to watch?" />
+
+      <Link to="/auth/signin">Sign In</Link>
     </header>
   );
 };
--- src/pages/app/home/Home.tsx
+++ src/pages/app/home/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "@components/Layout/Layout";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Layout>
+      <section>Hey</section>
+    </Layout>
+  );
 };
 
 export default Home;
