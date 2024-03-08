e798cb8b36a94b7f8609935c133c2b3b05a33e72
Housematics <houssem.developer@gmail.com>
2024-03-08 18:29:29 +0100

    update home page

--- src/pages/app/home/Home.tsx
+++ src/pages/app/home/Home.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import Layout from "@components/Layout/Layout";
 import "./Home.css";
 import Showcase from "@/components/showcase/Showcase";
+import TrendingMovies from "@components/rows/trending/Movies";
+import AllMovies from "@components/rows/normal/Movies";
 
 const Home: React.FC = () => {
   return (
     <Layout>
       <section className="home" id="home">
         <Showcase />
+        <TrendingMovies type="tv" title="Popular Series" />
+        <TrendingMovies type="movie" title="Popular Movies" />
+        <TrendingMovies type="all" title="All Trendings" />
+        <AllMovies type="tv" title="All Series" />
+        <AllMovies type="movie" title="All Movies" />
       </section>
     </Layout>
   );
