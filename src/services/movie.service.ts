2a68acee54f0875a9b587e003905829695d3355f
Housematics <houssem.developer@gmail.com>
2024-03-08 18:27:56 +0100

    update movie service

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -1,13 +1,26 @@
 import * as movieApi from "@api/movies.api";
-import type { ITrendsResp, Trending } from "@interfaces/movies.interface";
+import type {
+  IMoviesResp,
+  Trending,
+  Gender,
+} from "@interfaces/movies.interface";
 
 class MovieService {
-  async fetchTrendings(type: Trending): Promise<ITrendsResp> {
+  async fetchTrendings(type: Trending): Promise<IMoviesResp> {
     try {
       const { data } = await movieApi.fetchTrendings(type);
       return data;
     } catch (error: any) {
       throw new Error("cannot fetch trendings");
+    }
+  }
+
+  async fetchMovies(type: Gender, filter: string = ""): Promise<IMoviesResp> {
+    try {
+      const { data } = await movieApi.fetchMovies(type, filter);
+      return data;
+    } catch (error: any) {
+      throw new Error("cannot fetch movies");
     }
   }
 }
