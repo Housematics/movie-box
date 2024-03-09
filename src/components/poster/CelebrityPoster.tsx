bbd8afa275d3998527e955ab111d4bd86f547571
Housematics <houssem.developer@gmail.com>
2024-03-09 23:41:03 +0100

    add celebrity poster

---
+++ src/components/poster/CelebrityPoster.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./CelebrityPoster.css";
+import { ICelebrity } from "@/interfaces/movies.interface";
+import { Link } from "react-router-dom";
+import { TMDB_IMAGE_BASE_URL } from "@constants/index";
+
+interface Props {
+  celebrity: ICelebrity;
+}
+
+const CelebrityPoster: React.FC<Props> = ({ celebrity }) => {
+  return (
+    <div className="movie-poster">
+      <Link to="/">
+        <div className="poster-body">
+          <img
+            src={`${TMDB_IMAGE_BASE_URL + celebrity.profile_path}`}
+            alt="celebrity-image"
+          />
+
+          <h4>{celebrity.name || celebrity.original_name}</h4>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default CelebrityPoster;
