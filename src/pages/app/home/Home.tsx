757187f3ab55778edd956c0aff712467fffff9f7
Housematics <houssem.developer@gmail.com>
2024-03-09 23:36:36 +0100

    update home page

--- src/pages/app/home/Home.tsx
+++ src/pages/app/home/Home.tsx
@@ -4,6 +4,7 @@ import "./Home.css";
 import Showcase from "@/components/showcase/Showcase";
 import TrendingMovies from "@components/rows/trending/Movies";
 import AllMovies from "@components/rows/normal/Movies";
+import Celebrities from "@/components/rows/celebrities/Celebrities";
 
 const Home: React.FC = () => {
   return (
@@ -15,6 +16,7 @@ const Home: React.FC = () => {
         <TrendingMovies type="all" title="All Trendings" />
         <AllMovies type="tv" title="All Series" />
         <AllMovies type="movie" title="All Movies" />
+        <Celebrities title="Featured Casts" />
       </section>
     </Layout>
   );
