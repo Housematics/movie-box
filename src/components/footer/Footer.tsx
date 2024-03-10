6556dc7904277ec145a5a7ad9d5e25ca0a664f10
Housematics <houssem.developer@gmail.com>
2024-03-10 12:14:30 +0100

    add footer body

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,47 @@ import "./Footer.css";
 const Footer: React.FC = () => {
   return (
     <footer className="footer" id="footer">
-      Footer
+      <ul className="footer-icons">
+        <li>
+          <a href="">
+            <i className="fa-brands fa-square-facebook"></i>
+          </a>
+        </li>
+
+        <li>
+          <a href="">
+            <i className="fa-brands fa-square-instagram"></i>
+          </a>
+        </li>
+
+        <li>
+          <a href="">
+            <i className="fa-brands fa-square-twitter"></i>
+          </a>
+        </li>
+
+        <li>
+          <a href="">
+            <i className="fa-brands fa-youtube"></i>
+          </a>
+        </li>
+      </ul>
+
+      <ul>
+        <li>
+          <a href="">Conditions of Use</a>
+        </li>
+
+        <li>
+          <a href="">Privacy & Policy</a>
+        </li>
+
+        <li>
+          <a href="">Press Room</a>
+        </li>
+      </ul>
+
+      <p>© 2021 MovieBox by Adriana Eka Prayudha</p>
     </footer>
   );
 };
